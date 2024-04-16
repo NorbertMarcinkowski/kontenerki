e2130d41b3ddefeab0a947a36812ab95e78af412
NorbertMarcinkowski <norbert.marcinkowski@protonmail.com>
2024-04-16 14:05:38 +0200

    first commit

---
+++ src/main/java/org/example/piwkosklep/PiwkoSklepApplication.java
@@ -0,0 +1,13 @@
+package org.example.piwkosklep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiwkoSklepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiwkoSklepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/piwkosklep/PiwkoSklepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.piwkosklep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiwkoSklepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
